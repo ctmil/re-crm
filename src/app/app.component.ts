9a5583855dd5cc7d598e6c89346e39d934d27cb4
Ignacio Buioli <ibuioli@gmail.com>
2019-10-11 13:55:55 -0300

    feat: team with rpc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit {
   title = 're-crm';
 
   uid = 0;
-  serverUrl = 'http://157.230.61.194:8070/xmlrpc';
+  serverUrl = 'http://157.230.61.194:28069/xmlrpc';
 
   vendedor = '';
   @ViewChild('name', {static: false}) name: any;
@@ -37,6 +37,8 @@ export class AppComponent implements OnInit {
 
   login = false;
   alert = '';
+  success = false;
+  op_id = 0;
 
   equipos: Select[] = [];
   grupos: Select[] = [];
@@ -124,15 +126,35 @@ export class AppComponent implements OnInit {
           url: server + '/2/object',
           methodName: 'execute_kw',
           crossDomain: true,
-          params: [db, uid, pass, 'res.users', 'read', [response[0]], {fields: ['franchise_id', 'name']}],
+          params: [db, uid, pass, 'res.users', 'read', [response[0]], {fields: ['franchise_id', 'default_operating_unit_id', 'operating_unit_ids', 'name']}],
           success: (uresponse: any, ustatus: any, ujqXHR: any) => {
-            console.log(uresponse);
+            console.log('USER:', uresponse);
             if (uresponse[0][0]) {
               this.vendedor = uresponse[0][0].name;
+              this.op_id = uresponse[0][0].default_operating_unit_id[0];
               this.login = true;
             } else {
               this.login = false;
               this.alert = 'No está autorizado para acceder. Consulte al administrador.';
+            }
+
+            for (let i = 0; i <= uresponse[0][0].operating_unit_ids.length; i++) {
+              console.log(uresponse[0][0].operating_unit_ids[i]);
+              $.xmlrpc({
+                url: server + '/2/object',
+                methodName: 'execute_kw',
+                crossDomain: true,
+                params: [db, uid, pass, 'crm.team', 'search_read', [ [['operating_unit_id', '=', uresponse[0][0].operating_unit_ids[i]]] ], {fields: ['name', 'id']}],
+                success: (response: any, status: any, jqXHR: any) => {
+                  console.log(response);
+                  for (let i = 0; i < response[0].length; i++) {
+                    this.equipos.push( {value: response[0][i].id, viewValue: response[0][i].name} );
+                  }
+                },
+                error: (jqXHR: any, status: any, error: any) => {
+                  console.log('Error : ' + error );
+                }
+              });
             }
           },
           error: (ujqXHR: any, ustatus: any, uerror: any) => {
@@ -141,22 +163,6 @@ export class AppComponent implements OnInit {
             this.alert = 'No está autorizado para acceder. Consulte al administrador.';
           }
         });
-      },
-      error: (jqXHR: any, status: any, error: any) => {
-        console.log('Error : ' + error );
-      }
-    });
-
-    $.xmlrpc({
-      url: server + '/2/object',
-      methodName: 'execute_kw',
-      crossDomain: true,
-      params: [db, uid, pass, 'crm.team', 'search_read', [ [] ], {fields: ['name', 'id']}],
-      success: (response: any, status: any, jqXHR: any) => {
-        console.log(response);
-        for (let i = 0; i < response[0].length; i++) {
-          this.equipos[i] = {value: response[0][i].id, viewValue: response[0][i].name};
-        }
       },
       error: (jqXHR: any, status: any, error: any) => {
         console.log('Error : ' + error );
@@ -267,6 +273,7 @@ export class AppComponent implements OnInit {
       }]],
       success: (response: any, status: any, jqXHR: any) => {
         console.log(response);
+        this.success = true;
       },
       error: (jqXHR: any, status: any, error: any) => {
         console.log('Error : ' + error );
@@ -292,28 +299,28 @@ export class AppComponent implements OnInit {
     console.log('Tipo de Oportunidad: ' + this.typeOpp);
     console.log('Notas: ' + this.notes.nativeElement.value);
 
-    if (this.depLess50) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', '-50 Departamentos')].id); }
-    if (this.depMore50) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', '+50 Departamentos')].id); }
-    if (this.gAlta) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Gama Alta')].id); }
-    if (this.gBaja) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Gama Baja')].id); }
-    if (this.gMedia) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Gama Media')].id); }
-    if (this.premium) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Premium')].id); }
-    if (this.cerramiento) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Cerramiento')].id); }
-    if (this.cimiento) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Cimiento')].id); }
-    if (this.construccion) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Construccion')].id); }
-    if (this.mamposteria) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Mamposteria')].id); }
-    if (this.noIniciada) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'No Iniciada')].id); }
-    if (this.remodelacion) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Remodelacion')].id); }
-    if (this.cocinas) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Cocinas')].id); }
-    if (this.estar) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Estar')].id); }
-    if (this.lavadero) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Lavadero')].id); }
-    if (this.placares) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Placares')].id); }
-    if (this.vanitory) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Vanitory')].id); }
-    if (this.vestidor) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Vestidor')].id); }
-    if (this.madera) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Madera')].id); }
-    if (this.melamina) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Melamina')].id); }
-    if (this.polimero) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Polimero')].id); }
-    if (this.vidrio) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Vidrio')].id); }
+    if (this.depLess50) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', '-50 Departamentos')].id); }//
+    if (this.depMore50) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', '+50 Departamentos')].id); }//
+    if (this.gAlta) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Gama Alta')].id); }//
+    if (this.gBaja) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Gama Baja')].id); }//
+    if (this.gMedia) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Gama Media')].id); }//
+    if (this.premium) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Premium')].id); }//
+    if (this.cerramiento) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Cerramiento')].id); }//
+    if (this.cimiento) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Cimiento')].id); }//
+    if (this.construccion) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Construccion')].id); }//
+    if (this.mamposteria) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Mamposteria')].id); }//
+    if (this.noIniciada) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'No Iniciada')].id); }//
+    if (this.remodelacion) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Remodelacion')].id); }//
+    if (this.cocinas) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Cocinas')].id); }//
+    if (this.estar) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Estar')].id); }//
+    if (this.lavadero) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Lavadero')].id); }//
+    if (this.placares) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Placares')].id); }//
+    if (this.vanitory) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Vanitory')].id); }//
+    if (this.vestidor) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Vestidor')].id); }//
+    if (this.madera) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Madera')].id); }//
+    if (this.melamina) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Melamina')].id); }//
+    if (this.polimero) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Polimero')].id); }//
+    if (this.vidrio) {this.tagsList.push(this.tags[this.findWithAttr(this.tags, 'name', 'Vidrio')].id); }//
 
     this.writeCrm(this.serverUrl,
     this.getUrlParameter('db'),
